1e9d59018acacffa9d3f954402d01d43bb054b9a
XuanHuy <23110102@student.hcmute.edu.vn>
2026-06-01 13:31:05 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 python training/test.py \
   --detector_path training/config/detector/BiasLoraAsy.yaml \
-  --test_dataset "Celeb-DF-v2" \
-  --weights_path path/to/checkpoint.pth
+--test_dataset  "uniface_ff" "blendface_ff" "e4s_ff" "facedancer_ff" "fsgan_ff" "inswap_ff" "simswap_ff" \
+  --weights_path /kaggle/working/DeepfakeBench/training/pretrained/ckpt_best.pth
+
+#--test_dataset  "UADFV" "Celeb-DF-v2"  "DFDCP" "DeepFakeDetection" "DFDC" "uniface_ff" "blendface_ff" "e4s_ff" "facedancer_ff" "fsgan_ff" "inswap_ff" "simswap_ff" \
