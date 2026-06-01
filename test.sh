95aef152aaa645e13f4a21069116abf4dec440fa
Thanh Quân <phunglethanhquan1232005@gmail.com>
2026-06-01 13:31:52 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 python training/test.py \
   --detector_path training/config/detector/BiasLoraAsy.yaml \
---test_dataset  "uniface_ff" "blendface_ff" "e4s_ff" "facedancer_ff" "fsgan_ff" "inswap_ff" "simswap_ff" \
+  --test_dataset  "uniface_ff" "blendface_ff" "e4s_ff" "facedancer_ff" "fsgan_ff" "inswap_ff" "simswap_ff" \
   --weights_path /kaggle/working/DeepfakeBench/training/pretrained/ckpt_best.pth
 
 #--test_dataset  "UADFV" "Celeb-DF-v2"  "DFDCP" "DeepFakeDetection" "DFDC" "uniface_ff" "blendface_ff" "e4s_ff" "facedancer_ff" "fsgan_ff" "inswap_ff" "simswap_ff" \
