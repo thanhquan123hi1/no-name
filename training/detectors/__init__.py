95b2967cd0f5af918fdbfdd4c8d65cc76532c6bf
XuanHuy <23110102@student.hcmute.edu.vn>
2026-06-30 16:16:03 +0700

    Update __init__.py

--- training/detectors/__init__.py
+++ training/detectors/__init__.py
@@ -1,12 +1,16 @@
 import os
 import sys
+
 current_file_path = os.path.abspath(__file__)
 parent_dir = os.path.dirname(os.path.dirname(current_file_path))
 project_root_dir = os.path.dirname(parent_dir)
+
 sys.path.append(parent_dir)
 sys.path.append(project_root_dir)
 
 from metrics.registry import DETECTOR
+
 from .BiasLoraAsy import BiasLoraAsyDetector
 from .BiasRealAnchor import BiasRealAnchorDetector
 from .BiasConsistency import BiasConsistencyDetector
+from .SigLIPBiasConsistency import SigLIPBiasConsistencyDetector
